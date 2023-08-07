87d77c184f790ab787917d32defe8e90b1886a3c
동주 <dongjooyoung@naver.com>
2023-08-07 11:05:56 +0900

    백준 2884

---
+++ Algorithm/src/Algorithm/java/hanPotato2884.java
@@ -0,0 +1,31 @@
+package Algorithm.java;
+
+import java.util.Scanner;
+
+public class hanPotato2884 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int H = in.nextInt();
+		int M = in.nextInt();
+		
+		if (H > 0 && M >= 45) {
+			M = M-45;
+			System.out.println(H +" "+ M);
+		} else if (H > 0 && M < 45) {
+			H = H-1;
+			M = 60 - (45 - M);
+			System.out.println(H +" "+ M);
+		} else if (H == 0 && M >= 45) {
+			M = M-45;
+			System.out.println(H +" "+ M);
+		} else if (H == 0 && M < 45) {
+			H = 23;
+			M = 60 - (45 - M);
+			System.out.println(H +" "+ M);
+		}
+
+	}
+
+}
