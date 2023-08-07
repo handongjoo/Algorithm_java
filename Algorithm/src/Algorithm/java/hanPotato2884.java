b746f265d0dcc528ada02cab6c0be968f23ebf9e
동주 <dongjooyoung@naver.com>
2023-08-07 11:13:33 +0900

    백준 2884 수정

--- Algorithm/src/Algorithm/java/hanPotato2884.java
+++ Algorithm/src/Algorithm/java/hanPotato2884.java
@@ -12,19 +12,16 @@ public class hanPotato2884 {
 		
 		if (H > 0 && M >= 45) {
 			M = M-45;
-			System.out.println(H +" "+ M);
 		} else if (H > 0 && M < 45) {
 			H = H-1;
 			M = 60 - (45 - M);
-			System.out.println(H +" "+ M);
 		} else if (H == 0 && M >= 45) {
 			M = M-45;
-			System.out.println(H +" "+ M);
 		} else if (H == 0 && M < 45) {
 			H = 23;
 			M = 60 - (45 - M);
-			System.out.println(H +" "+ M);
-		}
+		} 			
+		System.out.println(H +" "+ M);
 
 	}
 
