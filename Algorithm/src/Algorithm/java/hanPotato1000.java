f3240914b0dac28c4c48f257a5895cb1b3e608a9
동주 <dongjooyoung@naver.com>
2023-08-07 09:46:07 +0900

    백준 1000번

---
+++ Algorithm/src/Algorithm/java/hanPotato1000.java
@@ -0,0 +1,17 @@
+package Algorithm.java;
+
+import java.util.Scanner;
+
+public class hanPotato1000 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int A = in.nextInt();
+		int B = in.nextInt();
+		
+		System.out.println(A + B);
+	}
+
+}
+
