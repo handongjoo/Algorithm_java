bd2344786841a9864ec4186933e5f11dcd949142
동주 <dongjooyoung@naver.com>
2023-08-07 10:15:06 +0900

    백준 10171

---
+++ Algorithm/src/Algorithm/java/hanPotato10171.java
@@ -0,0 +1,14 @@
+package Algorithm.java;
+
+public class hanPotato10171 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+
+	}
+
+}
