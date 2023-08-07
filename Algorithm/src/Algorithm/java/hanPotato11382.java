d636ae3e9d5b47aa6c3e61ef0c1159cebb10f4aa
동주 <dongjooyoung@naver.com>
2023-08-07 10:22:35 +0900

    백준 11382

---
+++ Algorithm/src/Algorithm/java/hanPotato11382.java
@@ -0,0 +1,18 @@
+package Algorithm.java;
+
+import java.util.Scanner;
+
+public class hanPotato11382 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		long A = in.nextLong();
+		long B = in.nextLong();
+		long C = in.nextLong();
+		
+		System.out.println(A+B+C);
+
+	}
+
+}
