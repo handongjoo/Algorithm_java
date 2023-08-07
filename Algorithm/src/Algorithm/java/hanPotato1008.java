e9e7206000402b87b0aeb8deba690d697976a272
동주 <dongjooyoung@naver.com>
2023-08-07 09:57:35 +0900

    백준 1008

---
+++ Algorithm/src/Algorithm/java/hanPotato1008.java
@@ -0,0 +1,19 @@
+package Algorithm.java;
+
+import java.util.Scanner;
+
+public class hanPotato1008 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int A = in.nextInt();
+		int B = in.nextInt();
+		
+		double result = (double) A/B;
+		
+		System.out.println(result);
+
+	}
+
+}
